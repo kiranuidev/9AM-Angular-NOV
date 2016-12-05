85fae1f82d0116c37c0d0d3e185e334df3e858d7
narasimha kbnd <narasimha kbnd>
2016-12-05 10:12:59 +0530

    Registration Component

--- DealersApp/src/app/app.module.ts
+++ DealersApp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
